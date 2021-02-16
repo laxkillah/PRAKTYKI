35d4c5e9d2584943bd993a1e4acb2f9d7f82dfe2
laxkillah <mzdrojewski93@gmail.com>
2021-02-16 20:55:48 +0100

    Koniec 2021-02-16 21:00

---
+++ Navigator2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navigator2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Navigator
+        {
+            string SetCurrentLocation();
+            string SetDestination();
+            string ModifyRouteToAvoid();
+            string ModifyRouteToInclude();
+            string GetRoute();
+            string GetTimeToDestination();
+            string TotalDistance();
+
+            string destination = textBox1.Text;
+            Navigator navigator1 = new Navigator();
+            navigator1.SetDestination(destination);
+            string route = navigator1.GetRoute
+
+
+
+        }
+            
+
+            
+    }
+}
