934f71607323c4de5be213722f9bed8624913730
Mariusz <78023200+laxkillah@users.noreply.github.com>
2021-02-16 16:51:06 +0100

    Add files via upload

---
+++ basen/basen/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace basen
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            int x = 0;
+            string poem = "";
+
+            while (x < 4)
+            {
+                poem = poem + "a";
+                if (x < 1)
+                {
+                    poem = poem + " ";
+                }
+                poem = poem + "n";
+
+                if (x > 1)
+                {
+                    poem = poem + " oyster";
+                    x = x + 2;
+                }
+                if (x == 1)
+                {
+                    poem = poem + "noys ";
+                }
+                if (x < 1)
+                {
+                    poem = poem + "oise ";
+                }
+
+                x = x + 1;
+            }
+            output.Text = poem;
+
+        }
+        
+        
+
+
+
+
+
+    }
+
+}
