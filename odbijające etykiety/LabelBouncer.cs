2fabab35ab69ef9a12e567c627e0d6b6367660f6
laxkillah <mzdrojewski93@gmail.com>
2021-02-20 15:14:17 +0100

    Koniec 2021-02-20 15:00

---
+++ odbijające etykiety/LabelBouncer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odbijające_etykiety
+{
+    class LabelBouncer
+    {
+        public Label myLabel;
+
+        public bool GoingForward = true;
+
+        public void Move()
+        {
+            if (myLabel != null)
+            {
+                if (GoingForward == true)
+                {
+                    myLabel.Left += 5;
+                    if (myLabel.Left >= myLabel.Parent.Width - myLabel.Width)
+                    {
+                        GoingForward = false;
+                    }
+                }
+                else
+                {
+                    myLabel.Left -= 5;
+                    if (myLabel.Left <= 0)
+                    {
+                        GoingForward = true;
+                    }
+                }
+            }
+
+        }
+    }
+}
