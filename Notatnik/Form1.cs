ae587744d821b0cbb5d2f6f0277b96a7486db20f
laxkillah <mzdrojewski93@gmail.com>
2021-03-04 19:59:00 +0100

    Koniec 2021-03-04 20:00

--- Notatnik/Form1.cs
+++ Notatnik/Form1.cs
@@ -17,17 +17,12 @@ namespace Notatnik
     
     public partial class Form1 : Form
     {
-
         string fileName = "";
-
-
 
         public Form1()
         {
             InitializeComponent();
-
-
-
+            
         }
 
 
@@ -80,15 +75,12 @@ namespace Notatnik
                 textBox.Text = f.ReadToEnd();
                 f.Close();
             }
-            textBox.Text = Decrypt(Decrypt(textBox.Text));;;
-
-
-
+            textBox.Text = Encrypt.DecryptString(textBox.Text, "test");
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            textBox.Text = Encrypt(Encrypt(textBox.Text));
+            textBox.Text = Encrypt.EncryptString(textBox.Text, "test");
             if (fileName != "")
             {
                 StreamWriter f = new StreamWriter(fileName);
@@ -96,13 +88,13 @@ namespace Notatnik
                 f.Close();
             }
             else saveAsToolStripMenuItem_Click(sender, e);
-            textBox.Text = Decrypt(Decrypt(textBox.Text));
+            textBox.Text = Encrypt.DecryptString(textBox.Text, "test");
         }
 
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
-            textBox.Text = Encrypt(Encrypt(textBox.Text));
+            RunEncryptInSeparateThread(Encrypt.EncryptString(textBox.Text);
+            textBox.Text = Encrypt.EncryptString(textBox.Text, "test");
             SaveFileDialog dialog = new SaveFileDialog();
             dialog.Filter = "Plik tekstowy (*.txt)|*.txt";
             dialog.ShowDialog();
@@ -110,56 +102,68 @@ namespace Notatnik
             {
                 
                 fileName = dialog.FileName;
-                StreamWriter f = new StreamWriter(fileName);
-                f.Write(textBox.Text);
-                f.Close();
+                StreamWriter streamWriter = new StreamWriter(fileName);
+                streamWriter.Write(textBox.Text);
+                streamWriter.Close();
             }
-            textBox.Text = Encrypt(Encrypt(textBox.Text));
+            textBox.Text = Encrypt.DecryptString(textBox.Text, "test");
         }
-
+        private static void RunEncryptInSeparateThread(Action action)
+        {
+            var thr = new Thread(new ThreadStart(action));
+            thr.Start();
+        }
 
         #region Encrypt & Decrypt
-
-        string Encrypt(string decrypted)
+        public static class Encrypt
         {
-            string hash = "test12345";
-            byte[] data = UTF8Encoding.UTF8.GetBytes(decrypted);
-
-
-            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-            TripleDESCryptoServiceProvider tripDES = new TripleDESCryptoServiceProvider();
-
-            tripDES.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
-            tripDES.Mode = CipherMode.ECB;
-
-            ICryptoTransform transform = tripDES.CreateEncryptor();
-            byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
-
-            return Convert.ToBase64String(result);
-
+            // This size of the IV (in bytes) must = (keysize / 8).  Default keysize is 256, so the IV must be
+            // 32 bytes long.  Using a 16 character string here gives us 32 bytes when converted to a byte array.
+            private const string initVector = "test";
+            // This constant is used to determine the keysize of the encryption algorithm
+            private const int keysize = 256;
+            //Encrypt
+            
+            public static string EncryptString(string plainText, string passPhrase)
+            {
+                
+                byte[] initVectorBytes = Encoding.UTF8.GetBytes(initVector);
+                byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+                PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null);
+                byte[] keyBytes = password.GetBytes(keysize / 8);
+                RijndaelManaged symmetricKey = new RijndaelManaged();
+                symmetricKey.Mode = CipherMode.CBC;
+                ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, initVectorBytes);
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                cryptoStream.FlushFinalBlock();
+                byte[] cipherTextBytes = memoryStream.ToArray();
+                memoryStream.Close();
+                cryptoStream.Close();
+                return Convert.ToBase64String(cipherTextBytes);
+            }
+            
+            public static string DecryptString(string cipherText, string passPhrase)
+            {
+                byte[] initVectorBytes = Encoding.UTF8.GetBytes(initVector);
+                byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+                PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null);
+                byte[] keyBytes = password.GetBytes(keysize / 8);
+                RijndaelManaged symmetricKey = new RijndaelManaged();
+                symmetricKey.Mode = CipherMode.CBC;
+                ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes);
+                MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+                byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+                int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                memoryStream.Close();
+                cryptoStream.Close();
+                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+            }
         }
-        string Decrypt(string encrypted)
-        {
-            string hash = "test12345";
-            byte[] data = Convert.FromBase64String(encrypted);
 
-            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-            TripleDESCryptoServiceProvider tripDES = new TripleDESCryptoServiceProvider();
-
-            tripDES.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
-            tripDES.Mode = CipherMode.ECB;
-
-            ICryptoTransform transform = tripDES.CreateDecryptor();
-            byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
-
-            return UTF8Encoding.UTF8.GetString(result);
-        }
-
-    
-
-        
         #endregion
-
         #region MenuItem
         private void undoToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -211,7 +215,7 @@ namespace Notatnik
         {
             textBox.Paste();
         }
-
         #endregion
+
     }
 }
---
+++ Notatnik/StorageKey.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notatnik
+{
+    class StorageKey : Form1
+    {
+        public static void KeyContainer()
+        {
+            try
+            {
+                // Create a key and save it in a container.
+                GenKey_SaveInContainer("MyKeyContainer");
+
+                // Retrieve the key from the container.
+                GetKeyFromContainer("MyKeyContainer");
+
+                // Delete the key from the container.
+                DeleteKeyFromContainer("MyKeyContainer");
+
+                // Create a key and save it in a container.
+                GenKey_SaveInContainer("MyKeyContainer");
+
+                // Delete the key from the container.
+                DeleteKeyFromContainer("MyKeyContainer");
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void GenKey_SaveInContainer(string containerName)
+        {
+            // Create the CspParameters object and set the key container
+            // name used to store the RSA key pair.
+            var parameters = new CspParameters
+            {
+                KeyContainerName = containerName
+            };
+
+            // Create a new instance of RSACryptoServiceProvider that accesses
+            // the key container MyKeyContainerName.
+            var rsa = new RSACryptoServiceProvider(parameters);
+
+            // Display the key information to the console.
+            Console.WriteLine($"Key added to container: \n  {rsa.ToXmlString(true)}");
+        }
+
+        private static void GetKeyFromContainer(string containerName)
+        {
+            // Create the CspParameters object and set the key container
+            // name used to store the RSA key pair.
+            var parameters = new CspParameters
+            {
+                KeyContainerName = containerName
+            };
+
+            // Create a new instance of RSACryptoServiceProvider that accesses
+            // the key container MyKeyContainerName.
+            var rsa = new RSACryptoServiceProvider(parameters);
+
+            // Display the key information to the console.
+            Console.WriteLine($"Key retrieved from container : \n {rsa.ToXmlString(true)}");
+        }
+
+        private static void DeleteKeyFromContainer(string containerName)
+        {
+            // Create the CspParameters object and set the key container
+            // name used to store the RSA key pair.
+            var parameters = new CspParameters
+            {
+                KeyContainerName = containerName
+            };
+
+            // Create a new instance of RSACryptoServiceProvider that accesses
+            // the key container.
+            var rsa = new RSACryptoServiceProvider(parameters)
+            {
+                // Delete the key entry in the container.
+                PersistKeyInCsp = false
+            };
+
+            // Call Clear to release resources and delete the key from the container.
+            rsa.Clear();
+
+            Console.WriteLine("Key deleted.");
+        }
+    }
+}
+
