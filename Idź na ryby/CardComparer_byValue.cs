8b94b2b78e63068c6823eca78daf1d48f5ceda73
laxkillah <mzdrojewski93@gmail.com>
2021-03-30 19:57:26 +0200

    Koniec 2021-03-30 20:00

---
+++ Idź na ryby/CardComparer_byValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idź_na_ryby
+{
+    public class CardComparer_byValue : IComparer<Card>
+    {
+        public int Compare(Card x, Card y)
+        {
+            if (x.Value < y.Value)
+                return -1;
+            if (x.Value > y.Value)
+                return 1;
+            if (x.Suit < y.Suit)
+                return -1;
+            if (x.Suit > y.Suit)
+                return 1;
+            return 0;
+        }
+    }
+}
