8d86060899e5a654192aaa5ae78c350f9dd841c7
laxkillah <mzdrojewski93@gmail.com>
2021-04-01 17:57:33 +0200

    Koniec 2021-04-01 18:00

---
+++ StreamWriter/Pineapple.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streamwrity
+{
+    public class Pineapple
+    {
+        const string d = "dostawa.txt";
+        public enum Fargo { North, South, East, West, Flamingo }
+        public static void Main(string[] args )
+        {
+            StreamWriter o = new StreamWriter("zamówienie.txt");
+            Pizza pz = new Pizza(new StreamWriter(d, true));
+            pz.Idaho(Fargo.Flamingo);
+            for (int w = 3; w >= 0; w--)
+            {
+                Pizza i = new Pizza(new StreamWriter(d, false));
+                i.Idaho((Fargo)w);
+                Party p = new Party(new StreamReader(d));
+                p.HowMuch(o);
+            }
+            o.WriteLine("To wszystko, kochani!");
+            o.Close();
+        }
+    }
+}
---
+++ StreamWriter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWrity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamWriter sw = new StreamWriter(@"c:\tajny_plan.txt");
+            //sw.WriteLine("W jaki sposób pokonać Kapitana Wspaniałego?");
+            //sw.WriteLine("Kolejny genialny, tajny plan Kanciarza.");
+            //sw.Write("Stworzę armię klonów, ");
+            //sw.WriteLine("uwolnię je i wystawię przeciwko mieszkańcom Obiektowa.");
+            //string location = "centrum handlowe.";
+            //for (int number = 0; number <= 6; number++)
+            //{
+            //    sw.WriteLine("Klon numer {0} atakuje {1}", number, location);
+            //    if (location == "centrum handlowe.") { location = "centrum miasta."; }
+            //    else { location = "centrum handlowe."; }
+            //}
+            //sw.Close();
+
+            //string folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            //StreamReader reader = new StreamReader(folder + @"\tajny_plan.txt");
+            //StreamWriter writer = new StreamWriter(folder + @"\e-maildoKapitanaWspaniałego.txt");
+
+            //writer.WriteLine("To: KapitanWspanialy@obiektowo.net");
+            //writer.WriteLine("From: Komisarz@obiektowo.net");
+            //writer.WriteLine("Subject: Czy możesz ocalić świat... po raz kolejny?");
+            //writer.WriteLine();
+            //writer.WriteLine("Odkryliśmy plan Kanciarza:");
+            //while (!reader.EndOfStream)
+            //{
+            //    string lineFromThePlan = reader.ReadLine();
+            //    writer.WriteLine("Plan -> " + lineFromThePlan);
+            //}
+            //writer.WriteLine();
+            //writer.WriteLine("Czy możesz nam pomóc?");
+            //writer.Close();
+            //reader.Close();
+        }
+
+    }
+}
